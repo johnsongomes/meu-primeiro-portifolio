c868052cd04370be7f26bd71db4d4dbdf8ccb9c0
johnsongomes <32249534+johnsongomes@users.noreply.github.com>
2026-03-25 18:36:11 -0300

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+// Suprime o alerta inicial para melhor
+window.alert("Olá, Desconhecido!");
+
+function mostrarMensagem() {
+    var nome = document.getElementById("nome").value.trim(); // Obtém o valor do campo de entrada
+    var resultado = document.getElementById("resultado");
+
+    if (!nome || nome.length === 0 || nome.length > 50 || /[^a-zA-Z\s]/.test(nome)) {
+        resultado.textContent = "Por favor, informe seu nome.";
+        resultado.style.color = "#B22222";
+        return;
+    }
+
+    resultado.textContent = "Olá, " + nome + "! Bem-vindo ao meu site.";
+    resultado.style.color = "#006400";
+    resultado.insertAdjacentHTML("beforeend", " <a href=\"https://www.youtube.com/@Johnsongomesof\" id=\"link-resultado\" target=\"_blank\">click aqui para me conhecer.</a>");}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    mostrarMensagem(); // Chama a função para mostrar a mensagem
+});
