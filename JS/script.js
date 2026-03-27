dd3805deabf573424c3481c94f50042588793fdf
johnsongomes <32249534+johnsongomes@users.noreply.github.com>
2026-03-27 12:53:48 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -11,7 +11,7 @@ function mostrarMensagem() {
         return;
     }
 
-    resultado.textContent = "Olá, " + nome + "! Bem-vindo ao meu site. click aqui --->>";
+    resultado.textContent = "Olá, " + nome + "! Bem-vindo ao meu site. click aqui -->>";
     resultado.style.color = "#006400";
     resultado.insertAdjacentHTML("beforeend", "<div><a href=\"https://github.com/johnsongomes\" class=\"text-white me-3\"><i class=\"bi bi-github\"></i></a><div><a href=\"https://www.youtube.com/@Johnsongomesof\" class=\"text-white me-3\"><i class=\"bi bi-youtube\"></i></a><a href=\"https://www.linkedin.com/in/johnson-gomes-19060295/\" class=\"text-white me-3\"><i class=\"bi bi-linkedin\"></i></a><a href=\"mailto:johnsongomes@gmail.com\" class=\"text-white\"><i class=\"bi bi-envelope\"></i></a></div>");}
 
