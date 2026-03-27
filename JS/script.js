ffef4f9d1bcd4b19432d763dbce2a9b4bc6ade92
johnsongomes <32249534+johnsongomes@users.noreply.github.com>
2026-03-27 14:20:34 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -33,7 +33,7 @@ window.scrollTo({ top: 0, behavior: 'smooth' });
 //script formulario de contato
  document.getElementById('form-contato').addEventListener('submit', function (e) {
      e.preventDefault();
-const nome = document.getElementById('nome').value;
+const nome = document.getElementById('nome2').value;
 const email = document.getElementById('email').value;
 const mensagem = document.getElementById('mensagem').value;
 alert(`Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.\n\nE-mail: 
